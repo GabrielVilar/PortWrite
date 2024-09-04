6a9ceb944d5cff97440fdb38f79cdc9160d80ee6
Gabriel Vilarinho <me.gabrielVilar@gmail.com>
2024-09-04 20:44:44 -0300

    Editing profile picture settings

--- profiles/static/js/cropper.js
+++ profiles/static/js/cropper.js
@@ -7,3 +7,24 @@ document.getElementById("cancel-btn").addEventListener("click", function(event) 
     event.preventDefault();
     document.getElementById("profile-picture-change-container").style.display = "none";
 });
+
+// JavaScript to handle image preview
+document.getElementById('file-input').addEventListener('change', function(event) {
+    const fileInput = event.target;
+    const file = fileInput.files[0];
+    const preview = document.getElementById('profile-picture-preview');
+    
+    if (file) {
+        const reader = new FileReader();
+
+        reader.onload = function(e) {
+            // Update the src attribute of the img tag to show the preview
+            preview.src = e.target.result;
+        }
+
+        reader.readAsDataURL(file); // Read the file as a data URL
+    } else {
+        // Reset to default image if no file is selected
+        preview.src = "{% static 'images/default_profile.png' %}";
+    }
+});
